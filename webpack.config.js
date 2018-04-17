ac9761d354b6abbcb8662179439a7d5a616cb3c5
Bart Kozal <bkzl@me.com>
2018-04-17 09:28:19 +0200

    Add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -6,5 +8,15 @@ module.exports = {
   output: {
     filename: "web.js",
     path: path.resolve(__dirname, "dist")
-  }
+  },
+  devServer: {
+    contentBase: "./dist",
+    open: "Google Chrome"
+  },
+  plugins: [
+    new CleanWebpackPlugin(["dist"]),
+    new HtmlWebpackPlugin({
+      title: "Games Tracker"
+    })
+  ]
 };
