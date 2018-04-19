388a11fb28c82d0e870bce2e9f3615862df66e8b
Bart Kozal <bkzl@me.com>
2018-04-19 23:23:53 +0200

    Set up styled components

--- web/Root.js
+++ web/Root.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import IndexPage from "./pages/index";
+import "./ui/global";
 
 const Root = () => (
   <Router>
