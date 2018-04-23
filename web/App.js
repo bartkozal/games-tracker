174793b8bb4f07fef96e96b7d8d90cc5f2eac350
Bart Kozal <bkzl@me.com>
2018-04-23 21:58:35 +0200

    Add placeholder for search page

--- web/App.js
+++ web/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import DiscoverPage from "pages/Discover";
+import SearchPage from "pages/Search";
 import Navbar from "features/Navbar";
 import Container from "ui/Container";
+import { ROOT_PATH, SEARCH_PATH } from "config/routes";
 import "config/css";
 
 const App = () => (
   <Router>
     <Container>
       <Navbar />
-      <Route exact path="/" component={DiscoverPage} />
+      <Route exact path={ROOT_PATH} component={DiscoverPage} />
+      <Route path={SEARCH_PATH} component={SearchPage} />
     </Container>
   </Router>
 );
