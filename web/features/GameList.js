5eeeb6ab876dfddd6654ca22303d6fe49b93aa13
Bart Kozal <bkzl@me.com>
2018-04-23 21:01:21 +0200

    Use times to generate game list

--- web/features/GameList.js
+++ web/features/GameList.js
@@ -1,30 +1,16 @@
 import React from "react";
+import { number } from "prop-types";
 import Flex from "../ui/Flex";
+import { times } from "lodash-es";
 
-const GameList = () => (
-  <Flex>
-    <div>
-      <img src="//picsum.photos/264/362/" style={{ maxWidth: "100%" }} />
-    </div>
-    <div>
-      <img src="//picsum.photos/264/362/" style={{ maxWidth: "100%" }} />
-    </div>
-    <div>
-      <img src="//picsum.photos/264/362/" style={{ maxWidth: "100%" }} />
-    </div>
-    <div>
-      <img src="//picsum.photos/264/362/" style={{ maxWidth: "100%" }} />
-    </div>
-    <div>
-      <img src="//picsum.photos/264/362/" style={{ maxWidth: "100%" }} />
-    </div>
-    <div>
-      <img src="//picsum.photos/264/362/" style={{ maxWidth: "100%" }} />
-    </div>
-    <div>
-      <img src="//picsum.photos/264/362/" style={{ maxWidth: "100%" }} />
-    </div>
-  </Flex>
+const GAME = <img src="//picsum.photos/128/175/" />;
+
+const GameList = ({ count = 7 }) => (
+  <Flex>{times(count, key => <div key={key}>{GAME}</div>)}</Flex>
 );
+
+GameList.propTypes = {
+  count: number
+};
 
 export default GameList;
