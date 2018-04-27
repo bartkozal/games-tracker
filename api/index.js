dcc1c8e52e82ee0b101b32f77e9383e873e1ffbf
Bart Kozal <bkzl@me.com>
2018-04-27 23:11:48 +0200

    Add some express middlewares

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 const express = require("express");
-const api = express();
+const bodyParser = require("body-parser");
+const compression = require("compression");
+const cors = require("cors");
+const morgan = require("morgan");
+const timeout = require("connect-timeout");
+const helmet = require("helmet");
 
 const API_PORT = process.env.PORT || 8080;
+const API_TIMEOUT = 40;
+
+const api = express();
+
+api.use(timeout(1000 * API_TIMEOUT));
+api.use(helmet());
+api.use(bodyParser.json());
+api.use(morgan("dev"));
+api.use(cors());
+api.use(compression());
 
 api.get("/", (_, res) => res.json({ version: "v1" }));
 
