a03e778591274c60979c085f67910576682880e7
Bart Kozal <bkzl@me.com>
2018-04-29 02:23:59 +0200

    Fix order of redux middlewares

--- web/config/store.js
+++ web/config/store.js
@@ -3,7 +3,7 @@ import logger from "redux-logger";
 import thunk from "redux-thunk";
 import rootReducer from "./rootReducer";
 
-const middlewares = [logger, thunk];
+const middlewares = [thunk, logger];
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
