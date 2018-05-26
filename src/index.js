6e7a93b3b3dfbe0f93a45f38bd11b74008b7542b
Bart Kozal <bkzl@me.com>
2018-05-26 14:08:55 +0200

    Add modern-normalize

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { combineReducers, createStore } from "redux";
 
+import "../node_modules/modern-normalize/modern-normalize.css";
+
 const rootReducer = combineReducers({});
 const store = createStore(rootReducer);
 
