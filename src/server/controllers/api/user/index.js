60492b3e09b42d1394c6bea8f761db8441feb5ca
Bart Kozal <bkzl@me.com>
2018-07-17 00:47:26 +0200

    Implement rating endpoint

--- src/server/controllers/api/user/index.js
+++ src/server/controllers/api/user/index.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import User from "../../../models/user";
+import Rating from "../../../models/rating";
 import { parseUserGames } from "./utils";
 
 const user = Router();
@@ -15,19 +16,26 @@ user.put("/games/:id", async (req, res) => {
   const user = await User.findById(req.user.id);
 
   // TODO
-  user.set("games", {
-    game: req.params.id,
-    platforms: req.body.platforms,
-    status: req.body.status
-  });
-
-  await user.save();
+  await user
+    .set("games", {
+      game: req.params.id,
+      platforms: req.body.platforms,
+      status: req.body.status
+    })
+    .save();
 
   res.status(204).end();
 });
 
 user.put("/games/:id/rating", async (req, res) => {
-  const user = await User.findById(req.user.id);
+  await Rating.update(
+    {
+      user: req.user.id,
+      game: req.params.id
+    },
+    { value: req.body.rating },
+    { upsert: true }
+  );
 
   res.status(204).end();
 });
