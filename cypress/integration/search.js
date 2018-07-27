6067fa9ea6fca8b123c0233447f5129c6eae3933
Bart Kozal <bkzl@me.com>
2018-07-27 09:07:23 +0200

    Rename detect to getId

--- cypress/integration/search.js
+++ cypress/integration/search.js
@@ -2,9 +2,9 @@ describe("Search", () => {
   it("works", () => {
     cy.visit("/");
 
-    cy.detect("search-input").type("witcher{enter}");
-    cy.detect("game-card-the-witcher-3-wild-hunt");
-    cy.detect("game-card-the-witcher-2-assassins-of-kings");
-    cy.detect("game-card-the-witcher");
+    cy.getId("search-input").type("witcher{enter}");
+    cy.getId("game-card-the-witcher-3-wild-hunt");
+    cy.getId("game-card-the-witcher-2-assassins-of-kings");
+    cy.getId("game-card-the-witcher");
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,1 +1,1 @@
-Cypress.Commands.add("detect", id => cy.get(`[data-test="${id}"]`));
+Cypress.Commands.add("getId", id => cy.get(`[data-test="${id}"]`));
