a7610e28f558ffb5bb2ca51038c7574757ec3a3e
Bart Kozal <bkzl@me.com>
2018-08-01 22:55:02 +0200

    Set up database connection

---
+++ src/server/config/db/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  development: {
+    username: "bkzl",
+    password: null,
+    database: "games-tracker-development",
+    host: "localhost",
+    dialect: "postgres"
+  }
+};
