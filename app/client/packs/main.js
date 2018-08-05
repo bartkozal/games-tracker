c5e9c4d889127dbca127dd8705ad6f24b87d33a4
Bart Kozal <bkzl@me.com>
2018-08-05 15:06:25 +0200

    Migrate client config files

--- app/client/packs/main.js
+++ app/client/packs/main.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 const Hello = props => <div>Hello {props.name}!</div>;
+
+Hello.propTypes = {
+  name: PropTypes.string
+};
 
 ReactDOM.render(<Hello name="World" />, document.getElementById("root"));
