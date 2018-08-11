f65a18cd982bfbc5e6ea3b6f772037c05d1fbe62
Bart Kozal <bkzl@me.com>
2018-08-11 15:53:52 +0200

    Add www to non-www redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # www to non-www redirect
+  match "(*any)",
+    to: redirect(subdomain: ""), via: :all, constraints: { subdomain: "www" }
+
   namespace :api, defaults: { format: :json } do
     namespace :user do
       resources :games, only: [:index, :update]
