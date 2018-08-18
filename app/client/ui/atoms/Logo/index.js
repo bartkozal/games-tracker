8eb034810c6a8db587bec8a4430bb8fa87d93d03
Bart Kozal <bkzl@me.com>
2018-08-18 13:57:32 +0200

    Add react-svg-loader

--- app/client/ui/atoms/Logo/index.js
+++ app/client/ui/atoms/Logo/index.js
@@ -1,6 +1,1 @@
-import React from "react";
-import logo from "./logo.svg";
-
-const Logo = () => <img src={logo} alt="Games Tracker" />;
-
-export default Logo;
+export { default } from "./logo.svg";
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,25 @@
 const { environment } = require("@rails/webpacker");
 
+// Insert react-svg-loader
+const babelLoader = environment.loaders.get("babel");
+environment.loaders.insert(
+  "svg",
+  {
+    test: /\.svg$/,
+    use: babelLoader.use.concat([
+      {
+        loader: "react-svg-loader",
+        options: {
+          jsx: true
+        }
+      }
+    ])
+  },
+  { after: "file" }
+);
+
+// Exclude .svg from file-loader
+const fileLoader = environment.loaders.get("file");
+fileLoader.exclude = /\.(svg)$/i;
+
 module.exports = environment;
