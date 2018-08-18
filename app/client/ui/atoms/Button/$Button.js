fcd2e710ec0f24be5786b2c7d5aaf65681eb7087
Bart Kozal <bkzl@me.com>
2018-08-18 13:47:43 +0200

    Update button padding

--- app/client/ui/atoms/Button/$Button.js
+++ app/client/ui/atoms/Button/$Button.js
@@ -1,19 +1,22 @@
 import styled from "react-emotion";
-import { SPACING_SMALL } from "ui/quarks";
+import { SPACING_SMALL, SPACING_BASE } from "ui/quarks";
 import { getButtonColors } from "./utils";
 
 const $Button = styled("button")(
   {
     border: "1px solid transparent",
-    padding: 0,
+    paddingTop: SPACING_SMALL,
+    paddingBottom: SPACING_SMALL,
+    paddingRight: SPACING_BASE,
+    paddingLeft: SPACING_BASE,
+    borderRadius: 4,
+
     ":hover": {
       cursor: "pointer"
     }
   },
   ({ modifier }) => ({
-    ...getButtonColors(modifier),
-    padding: SPACING_SMALL,
-    borderRadius: 4
+    ...getButtonColors(modifier)
   })
 );
 
