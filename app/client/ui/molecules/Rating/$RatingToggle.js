2d85372f20cf440ef022931171364e48dda013a5
Bart Kozal <bkzl@me.com>
2018-08-20 00:23:39 +0200

    Fix rate button

--- app/client/ui/molecules/Rating/$RatingToggle.js
+++ app/client/ui/molecules/Rating/$RatingToggle.js
@@ -2,6 +2,9 @@ import styled from "react-emotion";
 import { COLOR_ACCENT, FONT_LEAD } from "ui/quarks";
 
 const $RatingToggle = styled("div")({
+  height: 27,
+  display: "flex",
+  alignItems: "center",
   color: COLOR_ACCENT,
   fontSize: FONT_LEAD,
   textTransform: "uppercase",
