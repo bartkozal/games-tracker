91e72b6a6b899ed0c1bd11af38dd599cbe351bf7
Bart Kozal <bkzl@me.com>
2018-08-19 11:07:43 +0200

    Fix game card border radius

--- app/client/ui/organisms/GameCard/$GameCard.js
+++ app/client/ui/organisms/GameCard/$GameCard.js
@@ -1,10 +1,12 @@
 import styled from "react-emotion";
 import { COLOR_SECONDARY } from "ui/quarks";
 
+export const GAME_CARD_BORDER_RADIUS = 12;
+
 const $GameCard = styled("div")({
   position: "relative",
   backgroundColor: COLOR_SECONDARY,
-  borderRadius: 12
+  borderRadius: GAME_CARD_BORDER_RADIUS
 });
 
 export default $GameCard;
--- app/client/ui/organisms/GameCard/$GameCover.js
+++ app/client/ui/organisms/GameCard/$GameCover.js
@@ -1,11 +1,12 @@
 import styled from "react-emotion";
 import { SPACING_BASE } from "ui/quarks";
+import { GAME_CARD_BORDER_RADIUS } from "./$GameCard";
 
 const $GameCover = styled("div")({
   position: "relative",
   height: SPACING_BASE * 21,
-  borderTopLeftRadius: 12,
-  borderTopRightRadius: 12,
+  borderTopLeftRadius: GAME_CARD_BORDER_RADIUS,
+  borderTopRightRadius: GAME_CARD_BORDER_RADIUS,
   backgroundSize: "cover",
   backgroundPosition: "center",
 
@@ -17,6 +18,8 @@ const $GameCover = styled("div")({
     height: "100%",
     backgroundImage:
       "linear-gradient(rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 0) 36.35%)",
+    borderTopLeftRadius: GAME_CARD_BORDER_RADIUS,
+    borderTopRightRadius: GAME_CARD_BORDER_RADIUS,
     content: "''"
   }
 });
