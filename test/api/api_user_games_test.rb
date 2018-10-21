2d91e0566c02be3870d2e36dfe1d5b2c125e189d
Bart Kozal <bkzl@me.com>
2018-10-21 10:41:06 +0200

    Add spec for user games

---
+++ test/api/api_user_games_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class ApiUserGamesTest < ActionDispatch::IntegrationTest
+  test "GET /api/user/games (filter[id])" do
+    user_game_platforms = [Platform.find_by(slug: "PS4")]
+    user_game = create(:user_game, status: :backlog, rating: 8.0, platforms: user_game_platforms)
+
+    get api_user_games_path("filter[id]": user_game.game.id), headers: { Authorization: user_game.user.token }
+    response_body = JSON.parse(@response.body)
+
+    assert_not_empty response_body
+
+    assert_equal ["id", "name", "cover", "platforms", "rating", "status", "userPlatforms"], response_body.first.keys
+    assert_equal ["id", "slug"], response_body.first["platforms"].first.keys
+    assert_equal "backlog", response_body.first["status"]
+    assert_equal 8.0, response_body.first["rating"]
+    assert_equal "PS4", response_body.first["userPlatforms"].first["slug"]
+  end
+
+  test "PUT /api/user/game/:id" do
+    user_game = create(:user_game, status: :backlog)
+  end
+end
