34301055cb6af9d99cf1f29608a1b1aaa35e3b1b
Bart Kozal <bkzl@me.com>
2018-10-27 09:28:15 +0200

    Get rid off cssnext mixins

--- app/client/ui/components/Footer/index.js
+++ app/client/ui/components/Footer/index.js
@@ -3,7 +3,7 @@ import * as React from "react";
 import "./footer.css";
 
 const Footer = () => (
-  <ul className="footer">
+  <ul className="footer text-small">
     <li>Games Tracker API is powered by IGDB</li>
     <li>Privacy Policy</li>
   </ul>
