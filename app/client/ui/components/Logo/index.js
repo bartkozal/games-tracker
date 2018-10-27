977ed2e29059afa71f7ff77d4192e187d7b245b8
Bart Kozal <bkzl@me.com>
2018-10-27 09:15:38 +0200

    Migrate Logo

--- app/client/ui/components/Logo/index.js
+++ app/client/ui/components/Logo/index.js
@@ -1,7 +1,18 @@
 // @flow
 import * as React from "react";
+import cx from "classnames";
+import LogoSvg from "./logo.svg";
 
-// TODO
-const Logo = () => <div>Logo</div>;
+type Props = {
+  onClick?: Function
+};
+
+const Logo = ({ onClick }: Props) => (
+  <LogoSvg
+    data-cy="logo"
+    onClick={onClick}
+    className={cx({ "cursor-pointer": onClick })}
+  />
+);
 
 export default Logo;
