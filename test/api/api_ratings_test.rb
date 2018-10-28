969ddd024bf99bc103e73c937dd1ea813d3161f3
Bart Kozal <bkzl@me.com>
2018-10-28 18:36:57 +0100

    Update api specs

--- test/api/api_ratings_test.rb
+++ test/api/api_ratings_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class ApiRatingsTest < ActionDispatch::IntegrationTest
-  test "GET /api/ratings (filter[id])" do
+  test "GET /api/ratings (filter[id]) for a game" do
     user_game = create(:user_game, rating: 8)
 
     get api_ratings_path("filter[id]": user_game.game.id)
@@ -9,8 +9,20 @@ class ApiRatingsTest < ActionDispatch::IntegrationTest
 
     assert_not_empty response_body
 
+    assert_equal ["id", "score", "votes"], response_body.keys
+    assert_equal 8.0, response_body["score"]
+    assert_equal 1, response_body["votes"]
+  end
+
+  test "GET /api/ratings (filter[id]) for games" do
+    user_game_1 = create(:user_game, rating: 8)
+    user_game_2 = create(:user_game, rating: 10)
+
+    get api_ratings_path("filter[id]": [user_game_1.game.id, user_game_2.game.id].join(","))
+    response_body = JSON.parse(@response.body)
+
+    assert_not_empty response_body
+    assert_equal 2, response_body.size
     assert_equal ["id", "score", "votes"], response_body.first.keys
-    assert_equal 8.0, response_body.first["score"]
-    assert_equal 1, response_body.first["votes"]
   end
 end
--- test/api/api_user_games_test.rb
+++ test/api/api_user_games_test.rb
@@ -17,13 +17,13 @@ class ApiUserGamesTest < ActionDispatch::IntegrationTest
     assert_equal "PS4", response_body.first["userPlatforms"].first["slug"]
   end
 
-  test "GET /api/user/games (filter[id]) for a game with no status" do
+  test "GET /api/user/games (filter[id]) for a game with no status and platforms" do
     user_game = create(:user_game, rating: 5.0)
 
     get api_user_games_path("filter[id]": user_game.game.id), headers: { Authorization: user_game.user.token }
     response_body = JSON.parse(@response.body)
 
-    assert_equal "unassigned", response_body.first["status"]
+    assert_nil response_body.first["status"]
     assert_equal 5.0, response_body.first["rating"]
     assert_equal [], response_body.first["userPlatforms"]
   end
