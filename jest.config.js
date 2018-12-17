7f842a2bc0bd96a6ca8b761cc7c70e7e0169b100
Bart Kozal <bkzl@me.com>
2018-12-17 14:32:43 +0100

    Make jest work again

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   testURL: "http://localhost/",
   testMatch: ["**/app/client/**/?(*.)+(test).js"],
-  moduleDirectories: ["app/client", "node_modules"]
+  moduleDirectories: ["app/client", "node_modules"],
+  transform: {
+    "^.+\\.(js|jsx)$": "<rootDir>/node_modules/babel-jest"
+  }
 };
